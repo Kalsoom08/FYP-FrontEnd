ed19d0e08dd4e69ea38af61406efe3d5df38d401
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2025-07-17 09:34:10 +0500

    Alumni section is created

--- src/Pages/Public/Home.jsx
+++ src/Pages/Public/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {AlumniCard} from '../../Components/StatsCard/AlumniCard';
 
 export const Home = () => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+      <AlumniCard/>
     </div>
   );
 };
